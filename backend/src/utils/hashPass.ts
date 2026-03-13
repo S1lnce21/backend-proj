4d3731f6735dbd11d844b1e315309fa6daf251c7
S1lnce21 <sergeipinaev_2006@mail.ru>
2026-03-13 11:27:07 +0300

    backend

--- backend/src/utils/hashPass.ts
+++ backend/src/utils/hashPass.ts
@@ -3,3 +3,7 @@ import bcrypt from "bcrypt";
 export async function hashPass(pass: string): Promise<string> {
   return await bcrypt.hash(pass, 10);
 }
+
+export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
+  return bcrypt.compare(password, hashedPassword);
+};
